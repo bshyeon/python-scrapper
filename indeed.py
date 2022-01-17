67e5b1f863e1ac1a2e8550b6cb8977b35dcd6626
hyeon <liebe244@naver.com>
2022-01-17 21:58:22 +0900

    extract jobs pages

--- indeed.py
+++ indeed.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-    
-INDEED_URL = "https://kr.indeed.com/jobs?q=Python"
+
+LIMIT = 10
+URL = f"https://kr.indeed.com/jobs?q=Python&rqf=1&limit={LIMIT}"
 
 def extract_indeed_pages():    
     # html을 요청
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
 
     # class가 pagination인 div를 반환
@@ -20,3 +21,11 @@ def extract_indeed_pages():
     max_page = pages[-1]
     
     return max_page
+
+def extract_indeed_jobs(last_pages):
+    jobs = []
+    for page in range(last_pages):
+        result = requests.get(f"{URL}&start={LIMIT*page}")
+        print(result.status_code)
+        
+    return jobs
